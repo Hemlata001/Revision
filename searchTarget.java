25e603794d76677cbb9d328caf0aa6edbdb9652e
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-09-06 12:15:39 +0530

    Create searchTarget.java

---
+++ searchTarget.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main
+{
+  // Linear Search 
+  // time complexity - O(n)
+    static int linearSearch(int[] arr, int target){
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+  // binary Search
+  // Time complexity : O(logn)
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<= end){
+           int mid  = (start + end) /2;
+           if(arr[mid] == target){
+               return mid;
+           }else if(arr[mid] < target){
+               start = mid +1;
+           }else{
+               end = mid-1;
+           }
+       }
+       return -1;
+    }
+    
+	public static void main(String[] args) {
+	    int[] arr = {1,2,3,4};
+		int target = 3;
+		System.out.println("Linear Search : "+ linearSearch(arr,target));
+		System.out.println("Binary Search : "+ binarySearch(arr,target));
+	}
+}
