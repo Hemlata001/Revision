47d62a110dac143169cfffff0b87d0e23de9b936
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-09-15 16:56:16 +0530

    waveArray.java

--- waveArray.java
+++ waveArray.java
@@ -4,20 +4,12 @@ class Solution {
         
         for (int i = 0; i < n; i += 2) {
             // If current is smaller than previous, swap
-            if (i > 0 && arr[i] < arr[i - 1]) {
-                swap(arr, i, i - 1);
-            }
-            
-            // If current is smaller than next, swap
-            if (i < n - 1 && arr[i] < arr[i + 1]) {
-                swap(arr, i, i + 1);
-            }
+            if(i== n-1)break;
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
         }
     }
     
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+    
 }
