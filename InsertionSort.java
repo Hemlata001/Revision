3222e55940f4f199bca66103b74d5adf2613d822
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-01-15 15:09:06 +0530

    InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,36 @@
+
+public class Main
+{
+    public static void insertionSort(int[] arr){
+        for(int i = 1;i<arr.length;i++){
+            int curr = arr[i];
+            int prev = i-1;
+            
+            // finding out the correct position to insert
+            while(prev>=0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            
+            // insertion
+            arr[prev+1] = curr;
+        }
+    }
+    
+    public static void printArr(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    
+	public static void main(String[] args) {
+	    int[] arr = {5,4,1,3,2};
+		System.out.println("Before Insertion Sort");
+		printArr(arr);
+		System.out.println("After Insertion Sort");
+		insertionSort(arr);
+		printArr(arr);
+		
+	}
+}
