380acc1d797b0f0ad618942ffb1fbdb109dc77e6
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-01-29 18:51:09 +0530

    Implement isPowerOfTwo function in IsPower2.java

---
+++ IsPower2.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static boolean isPowerOfTwo(int n) {
+		return (n&(n-1)) == 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isPowerOfTwo(8));
+		System.out.println(isPowerOfTwo(11));
+
+	}
+}
