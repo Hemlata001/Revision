836597f232efb8be23c654174f18166d737cac53
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-04-08 17:01:17 +0530

    Create loop_counting_desc.java

---
+++ loop_counting_desc.java
@@ -0,0 +1,14 @@
+// print  counting from n to 1.
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter n: ");
+	    int n = sc.nextInt();
+	    
+	    for(int i = n;i>=1;i--){
+	        System.out.println(i);
+	    }
+	}
+}
