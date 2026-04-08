d9047beb1e6ea5bfd7e38f8708927b05453a58ac
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-04-08 17:33:44 +0530

    Create loop_even.java

---
+++ loop_even.java
@@ -0,0 +1,12 @@
+// print all even number from 1 to 100
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    for(int i = 0;i<=100;i++){
+	        if (i%2==0){
+	            System.out.println(i);
+	        }
+	    }
+	}
+}
