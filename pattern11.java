f6f8e4c0b554c630ea041dd8b0f8d68d1894922a
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-04-10 01:44:50 +0530

    Create pattern11.java

---
+++ pattern11.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter rows : ");
+		int n = sc.nextInt();
+		// part 1
+		for(int rows = 1;rows<=n;rows++){
+		    // spaces
+		    for(int cols= 1;cols<=n-rows;cols++){
+		        System.out.print("  ");
+		    }
+		    //star
+		    for(int cols=1;cols<=2*rows-1;cols++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		// part2
+		for(int rows = 1;rows<=n;rows++){
+		    // spaces
+		    if(rows==1){
+		        continue;
+		    }
+		    for(int cols=1;cols<= rows-1;cols++){
+		        System.out.print("  ");
+		    }
+		    // stars
+		    for(int cols = 1;cols<= 2*n-2*rows+1;cols++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
