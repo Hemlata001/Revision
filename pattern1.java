30d06b2bf7b8c0c8d04b3949b487b1cb1e893bfd
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-04-09 13:36:10 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter rows : ");
+		int n = sc.nextInt();
+		System.out.print("Enter columns : ");
+		int m = sc.nextInt();
+		for(int row = 1;row<=n;row++){
+		    for(int col = 1;col<= m;col++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	
+	}
+}
