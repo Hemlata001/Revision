c69bafacc1f4cb2df402e78e1017c82b4e44fee4
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2026-04-09 20:29:45 +0530

    Create pattern5.java

---
+++ pattern5.java
@@ -0,0 +1,18 @@
+// inverted right-angle triangle pattern
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter rows : ");
+		int n = sc.nextInt();
+		
+		for(int rows = 1;rows<=n;rows++){
+		    for(int cols = 1;cols<=n-rows+1;cols++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	
+	}
+}
